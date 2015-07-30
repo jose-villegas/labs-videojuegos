0fb32424db46b53dab1aa4922cf358b33be05544
jose-villegas <polonoise@gmail.com>
2015-07-29 22:27:16 -0430

    unity game, segment

---
+++ semana2/MiJuego/Assets/PlayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+	public float Health = 100.0f;
+	public float Speed = 10.0f;
+	// Movement control vars
+	private Vector3 _pointA;
+	public Vector3 PointB;
+	// Use this for initialization
+	void Start () {
+		_pointA = this.transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector3.Lerp(_pointA, PointB, Mathf.Cos(Time.time * Speed));
+	}
+}
